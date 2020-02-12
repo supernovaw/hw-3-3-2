40569ff14f1122bbd023c6f609ab882cbe2b4134
supernovaw <ruszrj@gmail.com>
2020-02-13 04:59:04 +1000

    Imports optimization

--- app/src/main/java/com/example/homework332/MainActivity.java
+++ app/src/main/java/com/example/homework332/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
